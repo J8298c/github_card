858785353fdadb1083a4d32bd3fe4f6b3f812fa3
j8298c <juliocmojica@gmail.com>
2017-03-18 20:19:47 -0400

    finished desktop styling

--- test/search.test.js
+++ test/search.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import { expect } from 'chai';
 
+
 import  Search  from '../js/components/search';
 const wrapper = shallow(<Search />);
+
 describe('<Search />', () => {
     it('should render a form with an onSubmit props', ()=> {
         expect(wrapper.find('FormGroup')).to.have.length(1);
@@ -20,8 +22,7 @@ describe('<Search />', () => {
     it('should render a button with an onClick props', ()=> {
         expect(wrapper.find('Button')).to.have.length(1);
         expect(wrapper.props().onClick).to.be.defined;
-    })
-    
+    }) 
 });
 
 
