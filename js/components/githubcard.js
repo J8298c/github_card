67b6d0fda53c371c89ed7d3b635df9a56df81831
j8298c <juliocmojica@gmail.com>
2017-03-20 22:34:16 -0400

    adding custom css removed bootstrap

--- js/components/githubcard.js
+++ js/components/githubcard.js
@@ -1,62 +1,71 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Media,Grid, Row, Col, Image} from 'react-bootstrap';
+
 
 export class GithubCard extends React.Component {
 
     render() {
         let { user } = this.props;
-        return (
-            <div className="userProfile container">
-                <Row>
-                    <Col xs={10} md={4}>
-                        <Image src={user.avatar_url} className="center-block userimage" thumbnail />
-                    </Col>
-                    <Row className="userInfo">
-                        <Col xs={12} md={6}>
-                            <p className="username">{user.name}</p>
-                        </Col>
-                        <Row>
-                            <Col xs={12} md={6}>
-                                <p className="userlocation">{user.location}</p>
-                            </Col>
-                            <Col xs={12} md={6}>
-                                <p className="userblog"><a href={user.blog}>My Portfolio</a></p>
-                            </Col>
-                            <Col xs={9} md={6}>
-                                <p className="userbio">{user.bio}</p>
-                            </Col>
-                            <Row>
-                                <Col xs={6} md={3}>
-                                    <div>
-                                        <p className="userfollowing">Following:
-                                        <a href={user.following_url}>{user.following}</a></p>
-                                    </div>
-                                </Col>
-                                <Col xs={6} md={3}>
-                                    <div>
-                                        <p className="userfollowers"> Followers:
-                                        <a href={user.follower_url}>{user.followers}</a></p>
-                                    </div>
-                                </Col>
-                                <Col xs={6} md={3}>
-                                    <div>
-                                        <p className="userrepo">User Repos
-                                        <a href={user.repos_url}>{user.public_repos}</a></p>
-                                    </div>
-                                </Col>
-                                <Col xs={6} md={3}>
-                                    <div>
-                                        <p className="usergist">User Gists
-                                        <a href={user.gists_url}>{user.public_gists}</a></p>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Row>
-                    </Row>
-                </Row>
-            </div>
+        return ( 
+            <div>
+                <div className="imageContainer">
+                <img src={user.avatar_url} className="userimg" />
+                </div>
+                <div className="profileContainer">
+                <h2 className="username">{user.name}</h2>
+                </div>
+            </div>          
         )
     }
 }
 export default connect()(GithubCard)
+
+ // <div className="userProfile container">
+ //                <Row>
+ //                    <Col xs={10} md={4}>
+ //                        <Image src={user.avatar_url} className="center-block userimage" thumbnail />
+ //                    </Col>
+ //                    <Row className="userInfo">
+ //                        <Col xs={12} md={6}>
+ //                            <p className="username">{user.name}</p>
+ //                        </Col>
+ //                        <Row>
+ //                            <Col xs={12} md={6}>
+ //                                <p className="userlocation">{user.location}</p>
+ //                            </Col>
+ //                            <Col xs={12} md={6}>
+ //                                <p className="userblog"><a href={user.blog}>My Portfolio</a></p>
+ //                            </Col>
+ //                            <Col xs={9} md={6}>
+ //                                <p className="userbio">{user.bio}</p>
+ //                            </Col>
+ //                            <Row>
+ //                                <Col xs={6} md={3}>
+ //                                    <div>
+ //                                        <p className="userfollowing">Following:
+ //                                        <a href={user.following_url}>{user.following}</a></p>
+ //                                    </div>
+ //                                </Col>
+ //                                <Col xs={6} md={3}>
+ //                                    <div>
+ //                                        <p className="userfollowers"> Followers:
+ //                                        <a href={user.follower_url}>{user.followers}</a></p>
+ //                                    </div>
+ //                                </Col>
+ //                                <Col xs={6} md={3}>
+ //                                    <div>
+ //                                        <p className="userrepo">User Repos
+ //                                        <a href={user.repos_url}>{user.public_repos}</a></p>
+ //                                    </div>
+ //                                </Col>
+ //                                <Col xs={6} md={3}>
+ //                                    <div>
+ //                                        <p className="usergist">User Gists
+ //                                        <a href={user.gists_url}>{user.public_gists}</a></p>
+ //                                    </div>
+ //                                </Col>
+ //                            </Row>
+ //                        </Row>
+ //                    </Row>
+ //                </Row>
+ //            </div>
--- js/components/search.js
+++ js/components/search.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { FormGroup, FormControl, InputGroup, Glyphicon, Button} from 'react-bootstrap';
+
 
 
 export default function Search(props){
     return(
         <div>
-        <FormGroup id="search" className="search-form" onSubmit={props.onSubmit}>
-            <InputGroup>
-            <FormControl className="search-input" type="text" placeholder="Enter your profile name" value={props.username}  onChange={props.onChange}
-                   onKeyPress={props.enterKey}
-            />
-            </InputGroup>
-            <Button bsStyle="primary" className="search-submit"  onClick={props.onSubmit}>Submit</Button>
-        </FormGroup>
+        <form id="search" className="search-form" onSubmit={props.onSubmit}>
+            <input className="searchinput" type="text" placeholder="Enter your profile name" value={props.username}  onChange={props.onChange}
+                onKeyPress={props.enterKey} />
+            <button className="submit"  onClick={props.onSubmit}>Submit</button>
+        </form>
         </div>
     )
 }
