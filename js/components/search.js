312d331bcc0ca93f1740a4b2403575756678c1d9
j8298c <juliocmojica@gmail.com>
2017-05-21 21:29:47 -0400

    fixing bundle and img source

--- js/components/search.js
+++ js/components/search.js
@@ -11,7 +11,7 @@ export default function Search(props){
             <button className="submit" onClick={props.onSubmit}>Submit</button>
         </form>
         <div className="banner-image">
-                <img src="/images/git.svg" alt="github logo" />
+                <img src="https://github.com/J8298c/github_card/blob/master/images/git.svg" alt="github logo" />
             </div>
         </div>
     )
