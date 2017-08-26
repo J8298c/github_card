c48e6c57be3498baac41f39551d4ea84495cc56e
j8298c <juliocmojica@gmail.com>
2017-08-26 16:50:25 -0400

    added eslintrc plus added actions to fetch user

---
+++ src/actions/index.js
@@ -0,0 +1,32 @@
+import { FETCH_USER_SUCCESS, FETCH_USER_ERROR } from './const';
+
+export function fetchUserSuccess(user) {
+  const action = {
+    type: FETCH_USER_SUCCESS,
+    user,
+  };
+  return action;
+}
+
+export function fetchUserError(bool) {
+  const action = {
+    type: FETCH_USER_ERROR,
+    bool: false,
+  };
+  return action;
+} 
+
+/*
+  fetch user from api request username is supplied
+  VIA the query parameter
+*/
+export default function fetchingUser(query) {
+  return (dispatch) => {
+    const url = `https://api.github.com/users/${query}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        dispatch(fetchUserSuccess(json));
+      }).catch(fetchUserError(true));
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import './index.css';
 import Routes from './Routes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+const initAppState = {};
+const store = createStore({}, initAppState, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider>
+    <Routes />
+  </Provider>, document.getElementById('root')
+);
 registerServiceWorker();
